4ba4248ea6b8f9e49a4dfcb2123670d4f33e0b1c
KannanN <kannan@webneo.in>
2017-07-05 14:55:31 +0530

    Calendar event selected and edit delete option for listing

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -405,7 +405,7 @@ export class CalendarPage {
           var event_date_array = this.eventIdentify[i]['event_date'].split('-');
           var yearstr = event_date_array[0];
           var monthstr = parseInt(event_date_array[1], 10)-1;
-          var datestr =  parseInt(event_date_array[2], 10);;
+          var datestr =  parseInt(event_date_array[2], 10);
           var startMinute = Math.floor(Math.random() * 24 * 60);
           var endMinute = Math.floor(Math.random() * 180) + startMinute;
 
