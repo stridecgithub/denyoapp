491826e1aceb3d0ee480489612d6f0507156337c
Harinivas <harinivas@webneo.in>
2017-07-08 12:04:03 +0530

    engine details completed

--- src/pages/engineview/engineview.ts
+++ src/pages/engineview/engineview.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import {NavController, NavParams } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Http, Headers, RequestOptions } from '@angular/http';
+//import { Http, Headers, RequestOptions } from '@angular/http';
+//import { HTTP } from '@ionic-native/http';
+import * as $ from 'jquery';
 
 
 
@@ -8,12 +13,42 @@ import {NavController, NavParams } from 'ionic-angular';
   templateUrl: 'engineview.html',
 })
 export class EngineviewPage {
+public pageTitle: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	public item = [];
+	public colorListArr = [];
+	iframeContent: any;
+	//private _inputpdf: string = '<iframe src="http://denyoappv2.stridecdev.com/2/1/unitdetails" height="350" frameborder="0"></iframe>';
+	private apiServiceURL: string = "http://denyoappv2.stridecdev.com";
+
+	public serviceCount;
+	public commentCount;
+
+
+
+
+	public unitDetailData: any = {
+model_id: '',
+		iframeURL: ''
+
+	
+
+	}
+	constructor(public http: Http, private sanitizer: DomSanitizer, public NP: NavParams, public navCtrl: NavController, public navParams: NavParams, public nav: NavController) {
+		this.unitDetailData.loginas = localStorage.getItem("userInfoName");
+		this.unitDetailData.userId = localStorage.getItem("userInfoId");
+
+
+	}
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EngineviewPage');
+  }
+  ionViewWillEnter() {
+    console.log(JSON.stringify(this.NP.get("record")));
+		let editItem = this.NP.get("record");
+    this.iframeContent = "<iframe id='filecontainer' src=" + this.apiServiceURL + "/webview_enginedetails/"+editItem.model_id + " height=500px width=100% frameborder=0></iframe>";
   }
 
 }
