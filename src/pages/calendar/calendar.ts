7fad17b7697a8e2c744c0c3258b65b0199e7974a
Harinivas <harinivas@webneo.in>
2017-08-10 16:31:11 +0530

    calendar and company detail issue fixed

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -326,7 +326,7 @@ export class CalendarPage {
         let months = { '01': 'January', '02': 'February', '03': 'March', '04': 'April', '05': 'May', '06': 'June', '07': 'July', '08': 'August', '09': 'September', '10': 'October', '11': 'November', '12': 'December' };
 
         let selDate = year + "-" + month + "-" + date;
-        let selectdate=date+"/"+month+"/"+year;
+        let selectdate=year+"-"+month+"-"+date;
         if (year != undefined)
           this.dateHeaderTitle = months[month] + " " + year;
         if (ev != '') {
